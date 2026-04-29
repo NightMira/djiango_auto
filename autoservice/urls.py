c3d930f01416ebbbf6503ee589916d12f31b5284
MirGame <mirgame@icloud.com>
2026-04-29 22:00:34 +0300

    add templates

--- autoservice/urls.py
+++ autoservice/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.core.views import home
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
 
